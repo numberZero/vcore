6178d87683d939ff2d14bf819d507654ef54a7f3
number Zero <silverunicorn2011@yandex.ru>
2018-05-25 01:04:32 +0300

    Add sanity checks

--- map/block.hxx
+++ map/block.hxx
@@ -6,6 +6,11 @@ static constexpr int block_size = 30;
 static constexpr int block_border = 1;
 static constexpr int block_size_pow2 = 32;
 
+static_assert ((block_size_pow2 & (block_size_pow2 - 1)) == 0,
+	  "block_size_pow2 must be a power of 2");
+static_assert (block_size + 2 * block_border == block_size_pow2,
+	  "Block size, together with the border, must be equal to its pow-2 size (block_size_pow2)");
+
 typedef std::uint16_t QubeType;
 typedef glm::ivec3 BlockPosition;
 typedef float LightLevel;
